bb59e4c00582d72ec120d01b71f051cd0da0da50
Leebecky <leebecky06@gmail.com>
2022-02-10 01:02:04 +0800

    Package Updates

--- WetMarketOnline/App_Start/BundleConfig.cs
+++ WetMarketOnline/App_Start/BundleConfig.cs
@@ -19,11 +19,13 @@ namespace WetMarketOnline
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                    "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
